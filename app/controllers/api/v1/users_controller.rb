b16a464d6bec10e6e3ed37e8b636c841f233a926
shashwatsapkota <shashwatsapkota09@gmail.com>
2019-08-18 22:40:34 +0545

    add users create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,21 @@ module Api
       def show
         respond_with User.find(params[:id])
       end
+
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: user.to_json, status: 201, location: [:api, user]
+        else
+          render json: { errors: user.errors }.to_json, status: 422
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 require 'api_constraints'
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'u'
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
     end
   end
 end
