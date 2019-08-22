15518bd4d9e6edadcb9cb9ea772d9de5a0f2bdd9
shashwatsapkota <shashwatsapkota09@gmail.com>
2019-08-22 21:04:05 +0545

    Add session controller create action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: %i[show create update destroy]
+      resources :sessions, only: %i[create destroy]
     end
   end
 end
