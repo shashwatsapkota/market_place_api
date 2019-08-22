e72a509fae6f152c5daa9f60ca27688671d7e2ac
shashwatsapkota <shashwatsapkota09@gmail.com>
2019-08-22 23:59:02 +0545

    Add authenticate with token to handle access to actions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -3,4 +3,10 @@ module Authenticable
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
+
+  def authenticate_with_token!
+    return if current_user.present?
+
+    render json: { errors: 'Not authenticated' }, status: :unauthorized
+  end
 end
--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 class Authentication
   include Authenticable
   def request; end
+
+  def response; end
 end
 
 describe Authenticable do
@@ -19,5 +21,21 @@ describe Authenticable do
     it 'returns the user from the authorization header' do
       expect(authentication.current_user.auth_token).to eql @user.auth_token
     end
+  end
+
+  describe '#authenticate with token' do
+    before do
+      @user = FactoryBot.create :user
+      allow(authentication).to receive(:current_user) { nil }
+      allow(response).to receive(:response_code) { 401 }
+      allow(response).to receive(:body) { { errors: 'Not authenticated' }.to_json }
+      allow(authentication).to receive(:response) { response }
+    end
+
+    it 'render a json error message' do
+      expect(json_response[:errors]).to eql 'Not authenticated'
+    end
+
+    it { should respond_with 401 }
   end
 end
