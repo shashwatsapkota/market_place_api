b0b1c09c3a536d5a5b20bf24589926ed34ad59d7
shashwatsapkota <shashwatsapkota09@gmail.com>
2019-08-22 23:19:54 +0545

    Add authenticable module for managing authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  # prevent CSRF attacks by raising an exception
+  # For APIs, you may want to use a null session instead
   protect_from_forgery with: :null_session
+
+  include Authenticable
 end
