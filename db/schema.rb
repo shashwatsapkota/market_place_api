cc94d993829f31ffee65401d9f20a5626affd5c4
shashwatsapkota <shashwatsapkota09@gmail.com>
2019-08-24 23:53:13 +0545

    Adds products barebones and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_163827) do
+ActiveRecord::Schema.define(version: 2019_08_24_170626) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "title", default: ""
+    t.decimal "price", default: "0.0"
+    t.boolean "published", default: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Product do
+  let(:product) { FactoryBot.build :product }
+  subject { product }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:price) }
+  it { should respond_to(:published) }
+  it { should respond_to(:user_id) }
+
+  it { should_not be_published }
+  it { should validate_presence_of :title }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  it { should validate_presence_of :published }
+  it { should validate_presence_of :user_id }
+end
