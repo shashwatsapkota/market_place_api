e30593a054f06819c28969e5274b3e2aff86e1b7
shashwatsapkota <shashwatsapkota09@gmail.com>
2019-08-30 22:56:51 +0545

    Adds search scopes on the product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,4 +15,64 @@ describe Product do
   it { should validate_presence_of :price }
   it { should validate_presence_of :user_id }
   it { should belong_to :user }
+
+  describe '.filter_by_title' do
+    before(:each) do
+      @product1 = FactoryBot.create :product, title: 'A plasma TV'
+      @product2 = FactoryBot.create :product, title: 'Fastest Laptop'
+      @product3 = FactoryBot.create :product, title: 'CD player'
+      @product4 = FactoryBot.create :product, title: 'LCD TV'
+    end
+
+    context "when a 'TV' pattern is sent" do
+      it 'returns the 2 products matching' do
+        expect(Product.filter_by_title('TV').size).to eq(2)
+      end
+
+      it 'returns the products matching' do
+        expect(Product.filter_by_title('TV')).to match_array([@product1, @product4])
+      end
+    end
+  end
+
+  describe '.above_or_equal_to_price' do
+    before(:each) do
+      @product1 = FactoryBot.create :product, price: 100
+      @product2 = FactoryBot.create :product, price: 50
+      @product3 = FactoryBot.create :product, price: 150
+      @product4 = FactoryBot.create :product, price: 99
+    end
+
+    it 'returns the products which are above or equal to the price' do
+      expect(Product.equal_or_above_to_price(100).sort).to match_array([@product1, @product3])
+    end
+  end
+
+  describe '.below_or_equal_to_price' do
+    before(:each) do
+      @product1 = FactoryBot.create :product, price: 100
+      @product2 = FactoryBot.create :product, price: 50
+      @product3 = FactoryBot.create :product, price: 150
+      @product4 = FactoryBot.create :product, price: 99
+    end
+
+    it 'returns the products which are below or equal to the price' do
+      expect(Product.below_or_equal_to_price(100).sort).to match_array([@product2, @product4, @product1])
+    end
+  end
+
+  describe '.recent' do
+    before(:each) do
+      @product1 = FactoryBot.create :product, price: 100
+      @product2 = FactoryBot.create :product, price: 50
+      @product3 = FactoryBot.create :product, price: 150
+      @product4 = FactoryBot.create :product, price: 99
+      @product2.touch
+      @product3.touch
+    end
+
+    it 'returns the most updated records' do
+      expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
+    end
+  end
 end
