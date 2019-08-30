466831220b4293085f940baee6508c553bd45862
shashwatsapkota <shashwatsapkota09@gmail.com>
2019-08-30 13:45:12 +0545

    Reverts before_validation back to before_create

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  before_validation :generate_authentication_token!
+  before_create :generate_authentication_token!
   validates :auth_token, uniqueness: true
 
   has_many :products, dependent: :destroy
