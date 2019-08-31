3cdb784bc095d799db2474048235068b114a22f9
shashwatsapkota <shashwatsapkota09@gmail.com>
2019-08-31 21:59:22 +0545

    Adds show action for order

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: %i[show create update destroy] do
         resources :products, only: %i[create update destroy]
-        resources :orders, only: %i[index]
+        resources :orders, only: %i[index show]
       end
       resources :sessions, only: %i[create destroy]
-      resources :products, only: %i[show index]
+      resources :products, only: %i[index show]
     end
   end
 end
--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -10,8 +10,24 @@ describe Api::V1::OrdersController, type: :controller do
     end
 
     it 'returns 4 order records from the user' do
-      orders_response = json_response
+      orders_response = json_response[:orders]
       expect(orders_response.size).to eql(4)
+    end
+
+    it { should respond_with 200 }
+  end
+
+  describe 'GET #show' do
+    before(:each) do
+      current_user = FactoryBot.create :user
+      api_authorization_header current_user.auth_token
+      @order = FactoryBot.create :order, user: current_user
+      get :show, params: { user_id: current_user.id, id: @order.id }
+    end
+
+    it 'returns the user order record matching the id' do
+      order_response = json_response[:order]
+      expect(order_response[:id]).to eql(@order.id)
     end
 
     it { should respond_with 200 }
